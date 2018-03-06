2797782320741f2c4689268610e7092e82b37216
Kohl Eisenschmidt <kohleisenschmidt@gmail.com>
2018-03-05 22:30:40 -0500

    having problems. VSC now says i have modified my file and now browser says cannot find file even though the file extentions are all correct.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,27 @@
 
 
 
+
 $(document).ready(function(){
 
-        var intervalId;
-        var time = 31;
-        var correct = 0;
-        var incorect =0;
-        var unanswered =0; 
-    
-        // $("#containter").on("click", "#startButton", function() {
-    
-        //     $("#containter").replaceWith("<div></div>");
-            
-            // alert("firstbutton");;
-        
-        
+  var intervalId;
+  var time = 11;
+  var correct = 0;
+  var incorrect =0;
+  var unanswered =5; 
+  window.location = '../../assets/otherhtmlpages/index3.html';
 
-        $("#startbutton").on("click", run);
-        $("#stop").on("click", stop);
+
+  // $("#containter").on("click", "#startButton", function() {
+
+  //     $("#containter").replaceWith("<div></div>");
+      
+      // alert("firstbutton");;
+  
+  
+
+  $("#startbutton").on("click", run);
+  $("#stop").on("click", stop);
 
         function run() {
             
@@ -26,47 +29,95 @@ $(document).ready(function(){
             intervalId = setInterval(decrement, 1000);
           }
 
-          //  The decrement function.
+
         function decrement() {
 
-        //  Decrease number by one.
         time--;
-  
-        //  Show the number in the #show-number tag.
+
         $("#clocktimer").html("<h2>" + time + "</h2>");
-  
-  
-        //  Once number hits zero...
+
+
         if (time === 0) {
-  
-          //  ...run the stop function.
+
           stop();
-  
-          //  Alert the user that time is up.
+
           alert("Time Up!");
           window.location.href = "../../assets/otherhtmlpages/index3.html";
         }
       }
-  
-      //  The stop function
-      function stop() {
-  
-        //  Clears our intervalId
-        //  We just pass the name of the interval
-        //  to the clearInterval function.
-        clearInterval(intervalId);
-      }
-  
-    //  Execute the run function.
-      run();
-         
 
-    });
-  
+        function stop() {
 
 
+          clearInterval(intervalId);
+        }
 
-
-
+        run();
+   
 
 // });
+     
+
+      
+        $("#queston2").click(function () {
+            var selectedValue = $("input[value='Plato']:checked").val();
+                      
+            if (selectedValue) {
+                correct++
+                $("#correctAnswers").text(correct);
+                alert("Hello, your favorite bird= " + selectedValue);
+            };
+          });
+
+            $("#queston2").click(function () {
+              var selectedValue = $("input[value='Achermus']:checked").val();
+                       
+              if (selectedValue) {
+                  incorrect++
+                  $("#incorrectAnswers").text(incorrect);
+                  
+              };
+
+          });
+            $("#queston2").click(function () {
+              var selectedValue = $("input[value='Nero']:checked").val();
+                        
+              if (selectedValue) {
+                  incorrect++
+                  $("#incorrectAnswers").text(incorrect);
+                 
+              };
+
+            });
+            $("#queston2").click(function () {
+            var selectedValue = $("input[value='Eugammon']:checked").val();
+                    
+            if (selectedValue) {
+                incorrect++
+                $("#incorrectAnswers").text(incorrect);
+                
+              };
+
+            });
+
+
+
+
+
+
+        $("#queston2").click(function () {
+          var selectedValue = $("checked").val();
+          // var selectedValue2 = $("input[value='Eugammon':checked").val();
+          
+          if (selectedValue) {
+
+              unanswered++
+              $("#incorrectAnswers").text(unanswered);
+              alert("Hello, your favorite bird= " + selectedValue);
+          };
+
+    });
+
+
+
+});
